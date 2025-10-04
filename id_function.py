c19b26974a89b379e4e1615ad29c2a54b2656df8
JigneshCalidad <jignesh.padhiyar@calidadinfotech.com>
2025-10-04 16:18:34 +0530

    [Add] functions

---
+++ id_function.py
@@ -0,0 +1,5 @@
+x = None
+y = x
+print(x)
+print(id(x)) #id function returns the identity of an object like the address of the object in memory
+print(id(y))# it will return same address
---
+++ type_function.py
@@ -0,0 +1,7 @@
+# The type() function returns the type of an object or variable
+c = 2 + 4j
+print(type(c))
+a = [10, 20, 30, 40, 50]
+print(a[3::-1]) #start, stop, step
+s2 = set([20, 30, 40])
+print(s2)
